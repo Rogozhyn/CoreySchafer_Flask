b69d52cd4fe191bec618cc7cd166555868f89133
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-04-05 00:11:20 +0300

    Second part of Blueprint implementation

--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -1,4 +1,10 @@
-from flask import Blueprint
+from flask import (render_template, url_for, flash,
+                   redirect, request, abort, Blueprint)
+from flask_login import current_user, login_required
+from flaskblog import db
+from flaskblog.models import Post
+from flaskblog.posts.forms import PostForm
+
 
 posts = Blueprint('posts', __name__)
 
@@ -51,5 +57,3 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
-
-
