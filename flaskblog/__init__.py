da3e098e695c9444f6cdf12095bf11b7c8f147f0
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-04-06 10:02:31 +0300

    Third part of Blueprint implementation. All works correctly

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -13,7 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'si
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = os.environ.get('EMAIL_SERV')
 app.config['MAIL_PORT'] = 587
@@ -22,10 +22,10 @@ app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
-from flaskblog.main.routes import main
 from flaskblog.users.routes import users
 from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
 
-app.register_blueprint(main)
 app.register_blueprint(users)
 app.register_blueprint(posts)
+app.register_blueprint(main)
